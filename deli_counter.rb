1c321291eec553ef3126be2117a53e4cf5c7076e
Ryan DeLong <rdelong111@gmail.com>
2022-05-30 08:43:10 -0500

    commit tree f4077eceb15d4d1d992ede3965e6ea10900307a7

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,25 @@
-# Write your code here.
+def line(theline)
+  if theline.length == 0
+    puts "The line is currently empty."
+  else
+    line = "The line is currently:"
+    theline.each_with_index do |current, index|
+      line.concat " #{index + 1}. #{current}"
+    end
+    puts line
+  end
+end
+
+def take_a_number(theline, name)
+  puts "Welcome, #{name}. You are number #{theline.length + 1} in line."
+  theline << name
+end
+
+def now_serving(theline)
+  if theline.length == 0
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{theline.shift}."
+  end
+  theline
+end
